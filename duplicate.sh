6f4ba0ea36952305e123b0f74a2bc377033b60a1
Joshua Powell <joshuapowell@users.noreply.github.com>
2023-03-13 16:24:01 -0400

    Create duplicate.sh

---
+++ duplicate.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+##
+# Create a mirror of an existing repository
+#
+# :param $1 The origin account on GitHub.com
+# :param $2 The origin repository name on GitHub.com
+# :param $3 The new or mirror repository name on GitHub.com
+#
+##
+
+# @todo Add check to see if the `git` command is available
+git clone --bare https://github.com/$1/$3.git;
+
+cd $3.git;
+
+# @todo Add check to see if the repository is available before attempting to push to it.
+git push --mirror https://github.com/$2/$3.git;
